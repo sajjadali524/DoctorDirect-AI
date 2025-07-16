8e222c2dac45afb8115b34ebc5d55483b4657842
Sajjad Ali <sajadalisk958@gmail.com>
2025-07-16 03:32:53 -0700

    api changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,4 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [react()],
 })
